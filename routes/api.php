1d454b340655c32851893b849db0353e85b01e77
AlbertoBFM <alberto.brandon2@gmail.com>
2022-04-04 22:31:52 -0400

    haciendo las rutas resource en api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\ArchiveController;
+use App\Http\Controllers\AssistanceController;
+use App\Http\Controllers\CommissionController;
+use App\Http\Controllers\DelegateController;
+use App\Http\Controllers\UniversityController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +24,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource( 'activity', ActivityController::class );
+Route::resource( 'archive', ArchiveController::class );
+Route::resource( 'assitance', AssistanceController::class );
+Route::resource( 'commission', CommissionController::class );
+Route::resource( 'delegate', DelegateController::class );
+Route::resource( 'university', UniversityController::class );
